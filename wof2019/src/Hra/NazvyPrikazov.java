eaadc8b92a603230d6c68f359b96555e349bc642
Ing. František Kajánek <kajanek6@fri.uniza.sk>
2019-03-13 09:18:21 +0100

    Derp

--- wof2019/src/Hra/NazvyPrikazov.java
+++ wof2019/src/Hra/NazvyPrikazov.java
@@ -13,7 +13,7 @@ package Hra;
 public class NazvyPrikazov {
     // konstantne pole nazvov prikazov
     private static final String[] PLATNE_PRIKAZY = {
-        "chod", "ukonci", "pomoc", "teleport", "pozri", "zober", "vypisInventar"
+        "chod", "ukonci", "pomoc", "teleport", "pozri", "zober", "vypisInventar", "pouziKluc"
     };
 
     /**
